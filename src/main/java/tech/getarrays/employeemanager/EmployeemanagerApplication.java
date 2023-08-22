3b28428ca26a7a8142ec3c6e086060e77aaf7551
neysho <azizamari100@gmail.com>
2023-08-22 09:44:02 +0100

    update

--- src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
@@ -20,7 +20,7 @@ public class EmployeemanagerApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://192.168.1.130:30170"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://emp.dev.neysho.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
